366631da4c2116d789751d1c7e2c41b9ee4e0a31
Caldwell <caldwellc1@appd.appstate.edu>
2017-11-10 14:54:01 -0500

    working but no to csv

--- World.py
+++ World.py
@@ -5,11 +5,11 @@ import numpy as np
 class World:
     def __init__(self):
         self.n = 0  # number of states
-        self.rewards = {}
-        self.states = {}
+        self.reward = {}
+        self.states = []
         self.terminal_states = set()
         self.number_directions = 0
-        self.directions_list = ['N', 'S', 'E', 'W']
+        self.directions_list = ['N', 'E', 'S', 'W']
         self.directions = {}
         self.m = 0  # number of edges
         self.world_map = {}
@@ -21,10 +21,10 @@ class World:
         file = open(name, "r")
         self.n = int(file.readline())
         for i in range(self.n):
-            state = file.readline()
-            state = state.rstrip('\n')
-            split = state.split(' ')
-            self.states.update({split[0]: int(split[1])})
+            reward = file.readline()
+            reward = reward.rstrip('\n')
+            split = reward.split(' ')
+            self.reward.update({split[0]: int(split[1])})
         terminal = file.readline()
         terminal = terminal.rstrip('\n')
         split = terminal.split(' ')
@@ -47,14 +47,19 @@ class World:
         self.gamma = float(file.readline())
         self.start_state = file.readline()
         file.close()
+        for words in self.reward:
+            if words not in self.terminal_states:
+                self.states.append(words)
         return
 
     # return a reward for a given state
     def R(self, state):
-        return self.rewards.get(state)
+        x = self.reward.get(state)
+        return self.reward.get(state)
 
     def T(self, state, action):
-        return self.directions[1], self.world_map[state + action]
+        action_take = [self.world_map[state + 'N'], self.world_map[state + 'E'], self.world_map[state + 'S'], self.world_map[state + 'W']]
+        return self.directions.get(action), action_take
 
     def actions(self, state):
         if state in self.terminal_states:
@@ -63,16 +68,36 @@ class World:
             return self.directions_list
 
 def value_iteration(mdp, epsilon=0.001):
-    U1 = {s: 0 for s in mdp.states}
+    U1 = mdp.reward.copy()
+    D1 = {s: ' ' for s in mdp.reward}
     R, T, gamma = mdp.R, mdp.T, mdp.gamma
+    eps_gam = epsilon * (1 - gamma) / gamma
     while True:
         U = U1.copy()
-        delta = 0
+        D = D1.copy()
+        delta = float(0)
         for s in mdp.states:
-            U1[s] = R(s) + gamma * max([sum([p * U[s1] for (p, s1) in T(s, a)]) for a in mdp.actions(s)])
+            temp = []
+            for a in mdp.actions(s):
+                p, sl = T(s,a)
+                temp.append(p[0] * U[sl[0]] + p[1] * U[sl[1]] + p[2] * U[sl[2]] + p[3] * U[sl[3]])
+            U1[s] = R(s) + gamma * max(temp)
+            D1[s] = getDirection(temp.index(max(temp)))
             delta = max(delta, abs(U1[s] - U[s]))
-        if delta < epsilon * (1 - gamma) / gamma:
-            return U
+        if delta <= eps_gam:
+            return U,D
+
+def getDirection(index):
+    if index == 0:
+        return 'N'
+    elif index == 1:
+        return 'E'
+    elif index == 2:
+        return 'S'
+    elif index == 3:
+        return 'W'
+    else:
+        return IndexError
 
 
 def main():
